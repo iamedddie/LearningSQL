e304b838d66c50817cee918274ddd1c91925ad3e
iamedddie <edyolisa@gmail.com>
2024-01-16 10:40:29 +0000

    setup

---
+++ main/java/com/example/demo/LearningSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSqlApplication.class, args);
+	}
+
+}
